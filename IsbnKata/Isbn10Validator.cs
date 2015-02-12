f88ea822f1a912dff653113a209bff4cccd60f04
Kevin Knowles <knowles.kevin@gmail.com>
2015-02-11 23:23:39 -0600

    fail commit

--- IsbnKata/Isbn10Validator.cs
+++ IsbnKata/Isbn10Validator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,7 +28,7 @@ namespace IsbnKata
 
         private static bool ContainsValidCharacters(string normalizedIsbn)
         {
-            return IsbnHelper.ContainsOnlyDigits(normalizedIsbn);
+            return IsbnHelper.ContainsOnlyDigits(normalizedIsbn.Last() == 'X' ? normalizedIsbn.Substring(0, 9) : normalizedIsbn);
         }
     }
 }
