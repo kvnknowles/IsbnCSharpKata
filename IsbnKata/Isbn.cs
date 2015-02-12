12dd401d46b4a262c977618a71c739045fd40e22
Kevin Knowles <knowles.kevin@gmail.com>
2015-02-11 23:10:03 -0600

    refactor commit

--- IsbnKata/Isbn.cs
+++ IsbnKata/Isbn.cs
@@ -18,30 +18,6 @@ namespace IsbnKata
             return new Isbn13Validator().IsValid(NormalizeIsbn());
         }
 
-        private static bool CheckSumMatchesCheckDigit(string normalizedIsbn)
-        {
-            var checkDigit = int.Parse(normalizedIsbn[12].ToString());
-            var checkSum = CalculateCheckSum(normalizedIsbn);
-            return checkSum == checkDigit;
-        }
-
-        private static int CalculateCheckSum(string normalizedIsbn)
-        {
-            var checkSum = 0;
-            for (var i = 1; i < 13; i++)
-            {
-                var multiplier = (IsEven(i)) ? EvenMultiplier : OddMultiplier;
-                checkSum += multiplier*int.Parse(normalizedIsbn[i - 1].ToString());
-            }
-            checkSum = (10 - (checkSum%10))%10;
-            return checkSum;
-        }
-
-        private static bool IsEven(int i)
-        {
-            return i%2 == 0;
-        }
-
         private static bool ContainsValidCharacters(string normalizedIsbn)
         {
             return normalizedIsbn.All(c => c >= '0' && c <= '9');
